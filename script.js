ef614ec0a79b90ac1e4bcffb2fe8064a19cf7484
Sarah Bühler <97050915+SarahBuehler@users.noreply.github.com>
2022-06-13 12:24:46 +0200

    add comments

--- script.js
+++ script.js
@@ -4,16 +4,14 @@ function changeImage() {
     const bisherigesImage = image.src;
     // console.log('bisherigesImate ' + bisherigesImage);
 
+    // bisherige img zwischenspeichern
     image.src = randomArrayItem(imagesArray);
     // console.log('image.src neu: ' + image.src);
+
+    // wenn das selbe img ausgewählt wird, dann nochmal aussuchen
     if (bisherigesImage === image.src) {
         image.src = randomArrayItem(imagesArray);
     }
-
-    /*     const newImage = randomArrayItem(imagesArray);
-        console.log('newImage: ' + newImage); // neuer imgage.src */
-
-
 }
 
 const imagesArray = ['img/pic01.jpg', 'img/pic02.jpg', 'img/pic03.jpg', 'img/pic04.jpg', 'img/pic05.jpg', 'img/pic06.jpg'];
