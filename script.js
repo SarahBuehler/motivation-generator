fb0246b7710ce3d541e6d1b8a557272ea5437d6f
Sarah Bühler <97050915+SarahBuehler@users.noreply.github.com>
2022-06-13 11:28:22 +0200

    add random pic switch

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function changeImage() {
     let image = document.getElementById('myImage');
-    if (image.src.match("img/weiß400x400.png")) {
+    /* if (image.src.match("img/transparent.png")) {
         image.src = "img/pic01.jpg";
     } else {
         image.src = "img/pic02.jpg";
-    }
+    } */
+    image.src = randomArrayItem(imagesArray);
 }
+
+const imagesArray = ['img/pic01.jpg', 'img/pic02.jpg', 'img/pic03.jpg', 'img/pic04.jpg', 'img/pic05.jpg', 'img/pic06.jpg'];
+
+function randomArrayItem(imagesArray) {
+    return imagesArray[Math.floor(Math.random() * imagesArray.length)];
+}
+
+console.log(randomArrayItem(imagesArray));
+console.log(randomArrayItem(imagesArray));
+console.log(randomArrayItem(imagesArray));
+console.log(randomArrayItem(imagesArray));
